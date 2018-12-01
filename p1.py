49305a7d40d8cfbdf8024b1388dc47a027dfa972
Michal Kowalik <mkowalik@kleiberweb.de>
2018-12-02 00:15:38 +0100

    puzzle 2 done

--- p1.py
+++ p1.py
@@ -1,7 +1,6 @@
 #%%
-import os
-import sys
 import functools
+import itertools
 
 #%%
 with open("input.txt") as f:
@@ -13,4 +12,16 @@ functools.reduce(lambda x, y: x + y, drifts, 0)
 
 #%% [markdown]
 ## 2'nd puzzle:
+def freq_generator():
+    fr = 0
+    while True:
+        for x in drifts:
+            fr = fr + x
+            yield fr
 
+frequencies = set([0])
+for current_frequency in freq_generator():
+    if current_frequency in frequencies:
+        print(current_frequency)
+        break
+    frequencies.add(current_frequency)

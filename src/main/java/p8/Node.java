cd5f97f7317ba01a591b46f7f4476c935aa1fffc
Michal Kowalik <mkowalik@kleiberweb.de>
2018-12-10 22:45:58 +0100

    day8 ready

--- src/main/java/p8/Node.java
+++ src/main/java/p8/Node.java
@@ -1,8 +1,14 @@
 package p8;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
   public List<Integer> metadata;
   public List<Node> childern;
+  
+  public Node() {
+    this.metadata = new ArrayList<>();
+    this.childern = new ArrayList<>();
+  }
 }
--- src/main/java/p8/P8.java
+++ src/main/java/p8/P8.java
@@ -1,7 +1,5 @@
 package p8;
 
-import jdk.jshell.spi.ExecutionControl;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,7 +9,9 @@ import java.util.stream.Collectors;
 
 public class P8 {
   
-  public static final String INPUT_PATH = "input8.txt";
+  private static final String INPUT_PATH = "input8.txt";
+  private int cursor = 0;
+  
   
   public static void main(String[] args) {
     P8 p8 = new P8();
@@ -22,21 +22,58 @@ public class P8 {
   private void solve1() {
     try {
       List<Integer> data = readInputData();
-      
-      // build the tree:
+
       Node tree = buildTree(data);
       
       // traverse the tree and get the sum of metadata:
-      
-      
-      System.out.println("done");
+      System.out.printf("Metadata sum: %d\n", getMetadataSum(tree));
+      System.out.printf("Root node value: %d\n", calculateRootValue(tree));
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
   
+  private int calculateRootValue(Node n) {
+    if (n.childern.isEmpty()) {
+      return n.metadata.stream().reduce(0, (x , y) -> x + y);
+    }
+    
+    return n.metadata.stream()
+            .map(p -> (p > n.childern.size()) ? 0 : calculateRootValue(n.childern.get(p - 1)))
+            .reduce(0, (x, y) -> x + y);
+  }
+  
+  private int getMetadataSum(Node n) {
+    int metadataSum = n.metadata.stream().reduce(0, (x, y) -> x + y);
+    if (n.childern.isEmpty()) {
+      return metadataSum;
+    }
+    
+    List<Integer> childrenSum =
+            n.childern.stream().map(this::getMetadataSum).collect(Collectors.toList());
+    
+    return metadataSum + childrenSum.stream().reduce(0, (x, y) -> x + y);
+  }
+  
   private Node buildTree(List<Integer> data) {
-    return null;
+    if (cursor >= data.size()) {
+      return null;
+    }
+    
+    Node n = new Node();
+    
+    int rootChildren = data.get(this.cursor++);
+    int rootMetadataSize = data.get(this.cursor++);
+    
+    for (int x = 0; x < rootChildren; x++) {
+      n.childern.add(buildTree(data));
+    }
+    
+    for (int x = 0; x < rootMetadataSize; x++) {
+      n.metadata.add(data.get(this.cursor++));
+    }
+    
+    return n;
   }
   
   
@@ -45,8 +82,8 @@ public class P8 {
     List<String> lines = Files.lines(Paths.get(INPUT_PATH))
             .collect(Collectors.toList());
   
-    return Arrays.asList(lines.get(0).split(" ")).stream()
-            .map(l -> Integer.parseInt(l)).collect(Collectors.toList());
+    return Arrays.stream(lines.get(0).split(" "))
+            .map(Integer::parseInt).collect(Collectors.toList());
   }
   
 }

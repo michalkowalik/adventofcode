70fb2874ccaa3f30b4f8df83e90b8543e5a43328
Michal Kowalik <mkowalik@kleiberweb.de>
2018-12-13 23:38:27 +0100

    day10 done

--- src/main/java/p10/P10.java
+++ src/main/java/p10/P10.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class P10 {
 
-  private static final String INPUT_FILE = "input10.test.txt";
+  private static final String INPUT_FILE = "input10.txt";
   static final String LINE_PATTERN =
           ".*<([ \\-\\d]+), ([ \\-\\d]+)>.*<([ \\-\\d]+), ([ \\-\\d]+)";
   
@@ -25,13 +27,65 @@ public class P10 {
       var pattern = Pattern.compile(LINE_PATTERN);
       var points = readInput(pattern);
       
+      var prevPoints = new ArrayList<Point>();
+      int area = getArea(points);
+      int modArea;
+      int iteration = 0;
       
+      while (true) {
+        prevPoints = (ArrayList<Point>) points.stream().collect(Collectors.toList());
+        points = nextView(points);
+        modArea = getArea(points);
+        if (modArea > area) {
+          System.out.printf("Solution at iteration %d\n", iteration);
+          break;
+        }
+        area = modArea;
+        iteration++;
+      }
       
-      System.out.println("done");
+      System.out.println(visualise(prevPoints));
+      
     } catch (IOException e) {
       e.printStackTrace();
     }
     
+  }
+  
+  private int getArea(List<Point> points) {
+    var miny = points.stream().map(p -> p.getY()).min((a, b) -> a > b ? 1 : -1);
+    var maxy = points.stream().map(p -> p.getY()).max((a, b) -> a > b ? 1 : -1);
+    return maxy.get() - miny.get();
+  }
+  
+  
+  // calculate next view:
+   List<Point> nextView(List<Point> p) {
+    return  p.stream().map(a -> new Point(
+            a.getX() + a.gethVelocity(),
+            a.getY() + a.getvVelocity(),
+            a.gethVelocity(),
+            a.getvVelocity())).collect(Collectors.toList());
+  }
+  
+  private String visualise(List<Point> points) {
+    var minx = points.stream().map(p -> p.getX()).min((a, b) -> a > b ? 1 : -1);
+    var maxx = points.stream().map(p -> p.getX()).max((a, b) -> a > b ? 1 : -1);
+    var miny = points.stream().map(p -> p.getY()).min((a, b) -> a > b ? 1 : -1);
+    var maxy = points.stream().map(p -> p.getY()).max((a, b) -> a > b ? 1 : -1);
+    
+    // table [row][column] ([y][x]) - easier to print!
+    String[][] table = new String[maxy.get() - miny.get() + 1][maxx.get() - minx.get() + 1];
+    
+    for (int row = 0; row < table.length; row++){
+      for (int col = 0; col < table[row].length; col++) {
+        table[row][col] = " ";
+      }
+    }
+    points.forEach(p -> table[p.getY() - miny.get()][p.getX() - minx.get()] = "*");
+  
+    List<String> x = Arrays.stream(table).map(p -> String.join("", p)).collect(Collectors.toList());
+    return String.join("\n", x);
   }
   
   private List<Point> readInput(Pattern pattern) throws IOException {
--- src/test/java/p10/P10Test.java
+++ src/test/java/p10/P10Test.java
@@ -3,6 +3,7 @@ package p10;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,5 +24,23 @@ class P10Test {
     assertEquals(-2, pp.getY());
     assertEquals(-1, pp.gethVelocity());
     assertEquals(1, pp.getvVelocity());
+  }
+  
+  
+  @Test
+  void nextView() {
+    var points = new ArrayList<Point>();
+    
+    points.add(new Point(9, 1, 0, 2));
+    
+    P10 p = new P10();
+    
+    var nextPoints = p.nextView(points);
+    
+    var modPoint = nextPoints.get(0);
+    
+    assertEquals(9, modPoint.getX());
+    assertEquals(3, modPoint.getY());
+    assertEquals(2, modPoint.getvVelocity());
   }
 }
